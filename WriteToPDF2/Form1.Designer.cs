7ba9bcf53b7e118be08b1d8837074be7d97a2bce
chenyu <chenyu@CURTIS-PC-055>
2016-12-16 16:23:07 -0500

    Add project files.

---
+++ WriteToPDF2/Form1.Designer.cs
@@ -0,0 +1,231 @@
+﻿namespace WriteToPDF2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.endTextBox = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.startTextBox = new System.Windows.Forms.TextBox();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.uploadFileTextBox = new System.Windows.Forms.TextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.printToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.exitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.label1 = new System.Windows.Forms.Label();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 487);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(519, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Click to Add Note";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(12, 311);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(519, 95);
+            this.textBox1.TabIndex = 1;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(9, 274);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(197, 17);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "PLEASE ENTER NOTE HERE";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(9, 163);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(533, 17);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "PLEASE ENTER PAGE RANGE YOU WANT TO PUT NOTE ON(INTERGER ONLY)";
+            // 
+            // endTextBox
+            // 
+            this.endTextBox.Location = new System.Drawing.Point(410, 208);
+            this.endTextBox.Name = "endTextBox";
+            this.endTextBox.Size = new System.Drawing.Size(81, 22);
+            this.endTextBox.TabIndex = 6;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(344, 208);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(25, 17);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "To";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(72, 208);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(90, 17);
+            this.label5.TabIndex = 8;
+            this.label5.Text = "FROM PAGE";
+            // 
+            // startTextBox
+            // 
+            this.startTextBox.Location = new System.Drawing.Point(197, 208);
+            this.startTextBox.Name = "startTextBox";
+            this.startTextBox.Size = new System.Drawing.Size(72, 22);
+            this.startTextBox.TabIndex = 9;
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // uploadFileTextBox
+            // 
+            this.uploadFileTextBox.Location = new System.Drawing.Point(12, 64);
+            this.uploadFileTextBox.Name = "uploadFileTextBox";
+            this.uploadFileTextBox.Size = new System.Drawing.Size(257, 22);
+            this.uploadFileTextBox.TabIndex = 10;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(298, 58);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(233, 34);
+            this.button2.TabIndex = 11;
+            this.button2.Text = "Upload PDF File";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fileToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(597, 28);
+            this.menuStrip1.TabIndex = 12;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // fileToolStripMenuItem
+            // 
+            this.fileToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.printToolStripMenuItem,
+            this.exitToolStripMenuItem});
+            this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+            this.fileToolStripMenuItem.Size = new System.Drawing.Size(44, 24);
+            this.fileToolStripMenuItem.Text = "File";
+            // 
+            // printToolStripMenuItem
+            // 
+            this.printToolStripMenuItem.Name = "printToolStripMenuItem";
+            this.printToolStripMenuItem.ShortcutKeys = ((System.Windows.Forms.Keys)((System.Windows.Forms.Keys.Control | System.Windows.Forms.Keys.P)));
+            this.printToolStripMenuItem.Size = new System.Drawing.Size(181, 26);
+            this.printToolStripMenuItem.Text = "Open";
+            this.printToolStripMenuItem.Click += new System.EventHandler(this.printToolStripMenuItem_Click);
+            // 
+            // exitToolStripMenuItem
+            // 
+            this.exitToolStripMenuItem.Name = "exitToolStripMenuItem";
+            this.exitToolStripMenuItem.ShortcutKeys = ((System.Windows.Forms.Keys)((System.Windows.Forms.Keys.Control | System.Windows.Forms.Keys.E)));
+            this.exitToolStripMenuItem.Size = new System.Drawing.Size(181, 26);
+            this.exitToolStripMenuItem.Text = "Exit";
+            this.exitToolStripMenuItem.Click += new System.EventHandler(this.exitToolStripMenuItem_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(9, 442);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(438, 17);
+            this.label1.TabIndex = 13;
+            this.label1.Text = "PLEASE CLOSE THE PDF BEFORE CLICK ADDING NOTE BUTTON";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(597, 602);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.uploadFileTextBox);
+            this.Controls.Add(this.startTextBox);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.endTextBox);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "PDF SMART ";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox endTextBox;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox startTextBox;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.TextBox uploadFileTextBox;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem fileToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem printToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem exitToolStripMenuItem;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ WriteToPDF2/Form1.cs
@@ -0,0 +1,120 @@
+﻿using System.Text;
+using iTextSharp.text.pdf;
+using iTextSharp.text.xml;
+using System;
+using System.IO;
+using System.Windows.Forms;
+using iTextSharp.text;
+using System.Diagnostics;
+
+namespace WriteToPDF2
+{
+    public partial class Form1 : Form
+    {
+        //variables
+        string pathin = "";
+        string pathout = "";
+        int pageNumber = 0;
+        string range = "";
+        int startPage = 0;
+        int endnotePage = 0;
+        public Form1()
+        {
+            InitializeComponent();
+            
+        }
+         
+private void button1_Click(object sender, EventArgs e)
+        {
+           
+            //get upload file path directory and set up new file location
+            pathin = openFileDialog1.FileName;
+            
+
+            pathout = Path.GetDirectoryName(pathin) + "/SOP Short Picking Ticket Form Note.pdf";
+            
+            try
+            {
+               
+           
+                //create a document object
+                var doc = new Document(PageSize.A4);
+
+                //create PdfReader object to read from the existing document
+                PdfReader reader = new PdfReader(pathin);
+                pageNumber = reader.NumberOfPages;
+                range = "1-" + pageNumber.ToString();
+                if (String.IsNullOrEmpty(endTextBox.Text))
+                {
+                    startPage = 1;
+                    endnotePage = pageNumber;
+                }
+                else
+                {
+                    startPage = Convert.ToInt32(startTextBox.Text);
+                    endnotePage = Convert.ToInt32(endTextBox.Text);
+                }
+                //select  pages from the original document
+                reader.SelectPages(range);
+
+                //create PdfStamper object to write to get the pages from reader 
+                PdfStamper stamper = new PdfStamper(reader, new FileStream(pathout, FileMode.Create));
+                // PdfContentByte from stamper to add content to the pages over the original content
+                
+                for (int i = startPage;  i <= endnotePage; i++)
+                {
+                    PdfContentByte pbover = stamper.GetOverContent(i);
+                    //add content to the page using ColumnText
+                    ColumnText.ShowTextAligned(pbover, Element.ALIGN_LEFT, new Phrase(textBox1.Text), 100, 400, 0);
+                    // PdfContentByte from stamper to add content to the pages under the original content
+                    PdfContentByte pbunder = stamper.GetUnderContent(pageNumber);
+                }
+                
+               
+            
+                //close the stamper
+                stamper.Close();
+                //close the reader
+                reader.Close();
+                //clean up
+                doc.Close();
+            }
+            catch (FormatException)
+            {
+                MessageBox.Show("please enter an interger");
+            }
+            MessageBox.Show("note added to the pdf");
+            Process.Start(pathout);
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            // Displays an OpenFileDialog so the user can select a Cursor.
+            
+            openFileDialog1.Filter = "PDF Files|*.pdf";
+            openFileDialog1.Title = "Select a PDF File";
+           
+            // If the user clicked OK in the dialog and
+            // a .pdf file was selected, open it.
+            if (openFileDialog1.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+            {
+                uploadFileTextBox.Text = openFileDialog1.SafeFileName;
+                pathin =openFileDialog1.FileName;
+            
+            }
+            
+
+        }
+
+        private void printToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            
+            Process.Start(pathout);
+        }
+
+        private void exitToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
